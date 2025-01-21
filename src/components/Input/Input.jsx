ac1e5b40917dc8aad8b4b75fa3e3bdb55bc5c48d
Katja Kibirieva <kkibirieva@gmail.com>
2025-01-21 17:30:02 +0200

    Update Input.jsx

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,9 @@
 import "./styles.css";
 
-function Input({name, placeholder}) {
+function Input({name, placeholder, type, label}) {
     return (
-        <input className= "main-input" type = "inputType" label = "input" name={name} placeholder={placeholder}/>
+        <input className= "main-input" type = "inputType" name={name} placeholder={placeholder}/>
+        <label>{label}</label>
     );
 
 }
